b7b11d7a0443e1855b10fc8e7332b2e9db008b34
Raphael <raphael.sonabend.15@ucl.ac.uk>
2018-12-20 16:17:05 +0000

    Create imdb_scraper.R

---
+++ imdb_scraper.R
@@ -0,0 +1,24 @@
+# To run just set the location to save the files to (file_loc) and select
+#   which of the IMDB datasets to download, default downloads all. Full details
+#   of the datasets can be found at https://www.imdb.com/interfaces/
+
+download_imdb_data = function(file_loc, namebasics = TRUE, akas = TRUE,
+                              titlebasics = TRUE, crew = TRUE, episode = TRUE,
+                              principals = TRUE, ratings = TRUE){
+
+  file_list = c("name.basics.tsv.gz",
+                "title.akas.tsv.gz",
+                "title.basics.tsv.gz",
+                "title.crew.tsv.gz",
+                "title.episode.tsv.gz",
+                "title.principals.tsv.gz",
+                "title.ratings.tsv.gz")
+  file_list = file_list[c(namebasics, akas, titlebasics, crew, episode, principals, ratings)]
+
+  for (f in file_list){
+    aURL = file.path("https://datasets.imdbws.com", f)
+    aPATH = file.path(file_loc, f)
+    download.file(aURL, aPATH)
+  }
+
+}
