0ca5f31a98c893742c0d6a2055556125ea4b27ee
Raphael <raphael.sonabend.15@ucl.ac.uk>
2018-12-20 14:08:39 +0000

    Bug fix

--- mpsontwitter_scraper.py
+++ mpsontwitter_scraper.py
@@ -2,85 +2,96 @@
 Script that utilises Beautiful Soup to scrape details of tweet interactions from UK MPs in a given date range.
  The data is saved both as a full data-set containing every single tweets interactions (not the tweet itself) and as
     an aggregated dataset, grouped for each MP. Interactions are defined as number of likes + number of retweets.
-
-Before running, set the start date and end date of interest and file location to save csvs.
 """
 
 import requests
 from bs4 import BeautifulSoup as bs
-import urllib.request
-import urllib
 import pandas as pd
-import numpy as np
-import re
 import time
 
 """"""
-#   Set date range of interest and location to save files here
 
-start_date = "08-06-2016"
-end_date = "08-06-2017"
-file_loc = "/Desktop/mpsontwitter/"
-""""""
+# Start_Date/End_Date - ("dd-mm-yyyy") to start/end scrape from
+# file_loc - Location to save csvs
+# likes - If True (default) saves number of likes for each tweet
+# retweets - If True (default) saves number of retweets for each tweet
+# interactions - If True (default) saves number of interactions (likes + retweets) for each tweet
 
-_URL = 'https://www.mpsontwitter.co.uk/archive/search?query=&literal=0&date_from='+start_date+'&date_to='+end_date+'&party=all&order=retweets'
-
-r = requests.get(_URL)
-soup = bs(r.text, "lxml")
-
-searchinfo = soup.findAll("div", attrs="search_results_info")
-numtweets = searchinfo[0].findAll("p")[0].contents[0]
-totaltweets = int(numtweets[6:len(numtweets)-len(" tweets matching the criteria ")].replace(",", ""))
-
-mps = []
-parties = []
-constituency = []
-interactions = []
-
-
-for i in range(0,totaltweets,50):
-    loop_start = time.time()
-
-    _URL = 'https://www.mpsontwitter.co.uk/archive/search?query=&literal=0&date_from=' + start_date + '&date_to=' + end_date + '&party=all&order=retweets&offset=' + str(i)
+def scrape_tweets(start_date, end_date, file_loc, likes=False, retweets=False, interactions=True):
+    _URL = 'https://www.mpsontwitter.co.uk/archive/search?query=&literal=0&date_from=' + start_date + '&date_to=' + end_date + '&party=all&order=retweets'
 
     r = requests.get(_URL)
     soup = bs(r.text, "lxml")
 
-    tweetdetails = soup.findAll("p", attrs="tweet_details text-muted")
-    mpdetails = soup.findAll("p", attrs="tweet_mp_details")
+    searchinfo = soup.findAll("div", attrs="search_results_info")
+    numtweets = searchinfo[0].findAll("p")[0].contents[0]
+    totaltweets = int(numtweets[6:len(numtweets) - len(" tweets matching the criteria ")].replace(",", ""))
 
-    for x in range(0,len(mpdetails)):
-        mp = mpdetails[x].findAll("span", attrs="bold")
-        party = mpdetails[x].findAll("span",attrs="badge")
-        if len(mpdetails[x].contents) < 6:
-            break
-        else:
-            con_end = mpdetails[x].contents[6][10:1000].find("\n")
-        if len(mp) == 0 or len(party) == 0 or con_end == -1:
-            break
-        else:
-            mps.append(mp[0].contents[0])
-            parties.append(party[0].contents[0])
-            constituency.append(mpdetails[x].contents[6][10:(10 + con_end)])
-            end = tweetdetails[x].contents[2].find("|")
-            retweets = int(tweetdetails[x].contents[2][1:end - 1].replace(",", ""))
-            end = tweetdetails[x].contents[4].find("|")
-            likes = int(tweetdetails[x].contents[4][1:end - 1].replace(",", ""))
-            interactions.append(retweets + likes)
+    mps = []
+    parties = []
+    constituency = []
+    lst_likes = []
+    lst_retweets = []
+    lst_interactions = []
 
-    loop_end = time.time()
-    time_dif = loop_end - loop_start
-    remaining = (time_dif * (324500-i)/50) / 60
-    print("There is roughly {0} minutes remaining".format(remaining))
-    print("Tweets scraped = " + str(i))
+    for i in range(0, totaltweets, 50):
+        time.sleep(.5)
+        loop_start = time.time()
 
-candidate_df = pd.DataFrame(
+        _URL = 'https://www.mpsontwitter.co.uk/archive/search?query=&literal=0&date_from=' + start_date + '&date_to=' + \
+               end_date + '&party=all&order=retweets&offset=' + str(i)
+
+        r = requests.get(_URL)
+        soup = bs(r.text, "lxml")
+
+        tweetdetails = soup.findAll("p", attrs="tweet_details text-muted")
+        mpdetails = soup.findAll("p", attrs="tweet_mp_details")
+
+        for x in range(0, len(mpdetails)):
+            mp = mpdetails[x].findAll("span", attrs="bold")
+            party = mpdetails[x].findAll("span", attrs="badge")
+            if len(mpdetails[x].contents) < 6:
+                continue
+            else:
+                con_end = mpdetails[x].contents[6][10:1000].find("\n")
+            if len(mp) == 0 or len(party) == 0 or con_end == -1:
+                continue
+            else:
+                mps.append(mp[0].contents[0])
+                parties.append(party[0].contents[0])
+                constituency.append(mpdetails[x].contents[6][10:(10 + con_end)])
+                end = tweetdetails[x].contents[2].find("|")
+                num_retweets = int(tweetdetails[x].contents[2][1:end - 1].replace(",", ""))
+                end = tweetdetails[x].contents[4].find("|")
+                num_likes = int(tweetdetails[x].contents[4][1:end - 1].replace(",", ""))
+                if likes:
+                    lst_likes.append(num_likes)
+                if retweets:
+                    lst_retweets.append(num_retweets)
+                if interactions:
+                    lst_interactions.append(num_retweets + num_likes)
+
+        loop_end = time.time()
+        time_dif = loop_end - loop_start
+        remaining = (time_dif * (totaltweets - int(i)) / 50) / 60
+        print("There is roughly {0:.2f} minutes remaining".format(remaining))
+        print("Percentage of tweets scraped = {0:04.2f}%".format(float((i / totaltweets))*100))
+
+    candidate_df = pd.DataFrame(
         {"MP": mps,
-         "Party" : parties,
-         "Constituency" : constituency,
-         "Interactions" : interactions})
+         "Party": parties,
+         "Constituency": constituency
+         }
+    )
 
-agg_df = candidate_df.groupby(['MP', 'Party', 'Constituency']).agg(['sum', 'min', 'max', 'mean', 'count']).round(2)
+    if interactions:
+        candidate_df["Interactions"] = lst_interactions
+    if likes:
+        candidate_df["Likes"] = lst_likes
+    if retweets:
+        candidate_df["Retweets"] = lst_retweets
 
-candidate_df.to_csv(file_loc+'Candidates')
-agg_df.to_csv(file_loc+'Candidates_Agg')
+    agg_df = candidate_df.groupby(['MP', 'Party', 'Constituency']).agg(['sum', 'min', 'max', 'mean', 'count']).round(2)
+
+    candidate_df.to_csv(file_loc + 'Candidates.csv')
+    agg_df.to_csv(file_loc + 'Candidates_Agg.csv')
